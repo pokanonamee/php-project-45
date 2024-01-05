bb479f1afd0452303ae2b27435bb5ed2f4d5a90d
EGOROV EGOR <you@example.com>
2024-01-05 18:35:13 +0500

    Сделал 5 шаг

---
+++ src/Games/MyProjectEven.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace BrainGames\Cli;
+
+function even()
+{
+    $name = run();
+    isEven($name);
+}
+
+function isEven(string $name)
+{
+    $finalAssocArray = [];
+
+    for ($i = 0; $i < 3; $i++) {
+        $number = rand(1, 100);
+        if ($number % 2 == 0) {
+            $finalAssocArray[$number] = 'yes';
+        } else {
+            $finalAssocArray[$number] = 'no';
+        }
+    }
+
+    $task = 'Answer "yes" if the number is even, otherwise answer "no".';
+    communication($finalAssocArray, $name, $task);
+}
